2e2deb37d7f6bde0591dc0ec305f3ac7617d0a4e
Andrian <andrian@royalsystems.dev>
2020-10-18 18:09:02 +0300

    added styles for view

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -18,6 +18,8 @@ class ContactServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__. '/config/contact.php' => config_path('contact.php'),
+            __DIR__ . '/views' => resource_path('views/pages'),
+            __DIR__.'/public' => public_path('vendor/contact')
         ], 'garstea-contact');
     }
 
--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -6,19 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contact Us</title>
+    <link rel="stylesheet" href="{{ asset('vendor/contact/css/style.css') }}">
 </head>
 <body>
 
 
-<h1>Contact Us any time</h1>
-
 <form action="{{ route('garstea.contact.send') }}" method="POST">
+    <h3 id="logo">Contact Us</h3>
     @csrf
     <input type="text" name="name" placeholder="Your Name">
     <input type="email" name="email" placeholder="Your Email">
     <textarea name="message" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
-    <button type="submit">Submit</button>
+    <input type="submit" value="Submit">
 </form>
+
 
 </body>
 </html>
